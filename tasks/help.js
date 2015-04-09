0b43383debb5b8e72ad2c1792249d7920bf29b6a
James Canning <james@jamescanning.com>
2015-04-09 12:20:09 +0100

    Add Speck version to help task

--- tasks/help.js
+++ tasks/help.js
@@ -3,7 +3,8 @@ module.exports = function(gulp, speck) {
     var gutil = require('gulp-util'),
       colors = gutil.colors;
 
-    gutil.log(colors.white.bold('SPECK') + ': guidelines for Grain sites.');
+    gutil.log(colors.white.bold('SPECK') + colors.grey(' v' + require('../package.json').version) + colors.italic(': guidelines for Grain sites.'));
+
     gutil.log('See ' + colors.underline.blue('http://speck.strapped.co/') + ' for documentation.');
   };
 
